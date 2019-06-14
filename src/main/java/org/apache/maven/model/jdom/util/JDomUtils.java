7a6603aa47da4c07da1295d8c9094163aae8a5b0
Eva Müller <emueller@coremedia.com>
2019-06-14 20:38:44 +0200

    Add new methods

--- src/main/java/org/apache/maven/model/jdom/util/JDomUtils.java
+++ src/main/java/org/apache/maven/model/jdom/util/JDomUtils.java
@@ -20,17 +20,25 @@ package org.apache.maven.model.jdom.util;
  */
 
 import org.codehaus.plexus.util.StringUtils;
+import org.jdom2.Comment;
 import org.jdom2.Content;
 import org.jdom2.Element;
 import org.jdom2.Parent;
 import org.jdom2.Text;
 import org.jdom2.filter.ElementFilter;
+import org.jdom2.util.IteratorIterable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
 import static java.lang.Math.max;
 import static java.util.Arrays.asList;
+import static org.apache.maven.model.jdom.util.JDomCfg.POM_ELEMENT_PROFILE;
+import static org.apache.maven.model.jdom.util.JDomCfg.POM_ELEMENT_PROFILES;
 
 /**
  * Common JDom functions
@@ -39,8 +47,49 @@ import static java.util.Arrays.asList;
  */
 public final class JDomUtils {
 
+  private static final Logger LOG = LoggerFactory.getLogger(JDomUtils.class);
+
   private JDomUtils() {
     // noop
+  }
+
+  public static int getElementIndex(Element element, Element root) {
+    return root.indexOf(element);
+  }
+
+  private static int getLastElementIndex(Element root) {
+    List<Element> elements = root.getContent(new ElementFilter());
+
+    int size = elements.size();
+    return size > 0 ? root.indexOf(elements.get(size - 1)) : -1;
+  }
+
+  /**
+   * Returns the given elements child element with the specified name.
+   *
+   * @param name   the name of the child element.
+   * @param parent the parent of the requested element - must not be {@code null}.
+   * @return the requested element or {@code null}.
+   */
+  public static Element getChildElement(String name, Element parent) {
+    return parent.getChild(name, parent.getNamespace());
+  }
+
+  /**
+   * Returns the trimmed text value of the given elements child element with the specified name.
+   *
+   * @param name   the name of the child element.
+   * @param parent the parent of the element whose text value is requested - must not be {@code null}.
+   * @return the trimmed text value of the element or {@code null}.
+   */
+  public static String getChildElementTextTrim(String name, Element parent) {
+    Element child = getChildElement(name, parent);
+    if (child == null) {
+      return null;
+    } else {
+      String text = child.getTextTrim();
+      return "null".equals(text) ? null : text;
+    }
   }
 
   /**
@@ -74,17 +123,6 @@ public final class JDomUtils {
     resetIndentations(element, detectIndentation(root) + "  ");
   }
 
-  public static int getElementIndex(Element element, Element root) {
-    return root.indexOf(element);
-  }
-
-  private static int getLastElementIndex(Element root) {
-    List<Element> elements = root.getContent(new ElementFilter());
-
-    int size = elements.size();
-    return size > 0 ? root.indexOf(elements.get(size - 1)) : -1;
-  }
-
   /**
    * Inserts a new child element to the given root element. The position where the element is inserted is calculated
    * using the element order that is defined in the {@link JDomCfg} (see {@link JDomCfg#getElementOrder(String)}).
@@ -99,6 +137,28 @@ public final class JDomUtils {
    */
   public static Element insertNewElement(String name, Element root) {
     return insertNewElement(name, root, calcNewElementIndex(name, root));
+  }
+
+  private static int calcNewElementIndex(String name, Element root) {
+    int addIndex = 0;
+
+    List<String> elementOrder = JDomCfg.getInstance().getElementOrder(root.getName());
+    if (elementOrder == null) {
+      addIndex = max(0, getLastElementIndex(root) + 1);
+    } else {
+      for (int i = elementOrder.indexOf(name) - 1; i >= 0; i--) {
+        String addAfterElementName = elementOrder.get(i);
+        if (!addAfterElementName.equals("")) {
+          Element addAfterElement = root.getChild(addAfterElementName, root.getNamespace());
+          if (addAfterElement != null) {
+            addIndex = root.indexOf(addAfterElement) + 1;
+            break;
+          }
+        }
+      }
+    }
+
+    return addIndex;
   }
 
   /**
@@ -125,103 +185,6 @@ public final class JDomUtils {
     return newElement;
   }
 
-  private static int calcNewElementIndex(String name, Element root) {
-    int addIndex = 0;
-
-    List<String> elementOrder = JDomCfg.getInstance().getElementOrder(root.getName());
-    if (elementOrder == null) {
-      addIndex = max(0, getLastElementIndex(root) + 1);
-    } else {
-      for (int i = elementOrder.indexOf(name) - 1; i >= 0; i--) {
-        String addAfterElementName = elementOrder.get(i);
-        if (!addAfterElementName.equals("")) {
-          Element addAfterElement = root.getChild(addAfterElementName, root.getNamespace());
-          if (addAfterElement != null) {
-            addIndex = root.indexOf(addAfterElement) + 1;
-            break;
-          }
-        }
-      }
-    }
-
-    return addIndex;
-  }
-
-  private static boolean isBlankLineBeforeElement(String name, Element root) {
-    List<String> elementOrder = JDomCfg.getInstance().getElementOrder(root.getName());
-    return elementOrder != null && elementOrder.get(max(0, elementOrder.indexOf(name) - 1)).equals("");
-  }
-
-  /**
-   * Tries to detect the indentation that is used within the given element and returns it.
-   * <p>
-   * The method actually returns all characters (supposed to be whitespaces) that occur after the last linebreak in a
-   * text element that is followed by an XML element.
-   *
-   * @param element the element whose contents should be used to detect the indentation.
-   * @return the detected indentation or {@code null} if not indentation can be detected.
-   */
-  public static String detectIndentation(Element element) {
-    String indent = null;
-
-    String indentCandidate = null;
-    for (Content childElement : element.getContent()) {
-      if (childElement instanceof Text) {
-        String text = ((Text) childElement).getText();
-        int lastLsIndex = StringUtils.lastIndexOfAny(text, new String[]{"\n", "\r"});
-        if (lastLsIndex > -1) {
-          indentCandidate = text.substring(lastLsIndex + 1);
-        }
-      } else if (indentCandidate != null) {
-        if (childElement instanceof Element) {
-          indent = indentCandidate;
-          break;
-        } else {
-          indentCandidate = null;
-        }
-      }
-    }
-
-    if (indent == null) {
-      Parent parent = element.getParent();
-      if (parent instanceof Element) {
-        indent = detectIndentation((Element) parent) + "  ";
-      } else {
-        return "";
-      }
-    }
-
-    return indent;
-  }
-
-  /**
-   * Returns the given elements child element with the specified name.
-   *
-   * @param name   the name of the child element.
-   * @param parent the parent of the requested element - must not be {@code null}.
-   * @return the requested element or {@code null}.
-   */
-  public static Element getChildElement(String name, Element parent) {
-    return parent.getChild(name, parent.getNamespace());
-  }
-
-  /**
-   * Returns the trimmed text value of the given elements child element with the specified name.
-   *
-   * @param name   the name of the child element.
-   * @param parent the parent of the element whose text value is requested - must not be {@code null}.
-   * @return the trimmed text value of the element or {@code null}.
-   */
-  public static String getChildElementTextTrim(String name, Element parent) {
-    Element child = getChildElement(name, parent);
-    if (child == null) {
-      return null;
-    } else {
-      String text = child.getTextTrim();
-      return "null".equals(text) ? null : text;
-    }
-  }
-
   /**
    * Remove a child element from the parent.
    *
@@ -236,6 +199,90 @@ public final class JDomUtils {
       parent.removeContent(index);
     }
     removeChild.detach();
+  }
+
+  /**
+   * Remove a child of type {@link Content} from the parent.
+   *
+   * @param parent      the parent element.
+   * @param removeChild the child content to be removed.
+   */
+  public static void removeChildContent(Element parent, Content removeChild) {
+    int index = parent.indexOf(removeChild);
+    if (index >= 0) {
+      LOG.debug("");
+      LOG.debug("index [{}] => REMOVE: {}", index, contentToString(parent.getContent(index)));
+      parent.removeContent(index);
+      index--;
+      // remove new line
+      if (index >= 0 && hasNewlines(parent.getContent(index))) {
+        LOG.debug("NL    [{}] => REMOVE: {}", index, contentToString(parent.getContent(index)));
+        simpleRemoveAtIndex(index, parent, false);
+
+        int prevIndex = index - 1;
+        if (prevIndex >= 0) {
+          // remove comment
+          LOG.debug("\t@[{}] => {}", prevIndex, contentToString(parent.getContent(prevIndex)));
+          int newIndex = removeCommentAtIndex(prevIndex, parent);
+          if (newIndex >= 0) {
+            LOG.debug("\t*[{}] => {}", newIndex, contentToString(parent.getContent(newIndex)));
+          }
+          while (newIndex >= 0 && isComment(parent.getContent(newIndex))) {
+            LOG.debug("\t*[{}] => ALSO REMOVE: {}", newIndex, contentToString(parent.getContent(newIndex)));
+            newIndex = removeCommentAtIndex(newIndex, parent);
+            if (newIndex >= 0) {
+              LOG.debug("\t [{}] => NEXT       : {}", newIndex, contentToString(parent.getContent(newIndex)));
+            }
+            if (newIndex >= 0 && isMultiNewLine(parent.getContent(newIndex - 1))) {
+              break;
+            }
+          }
+        }
+      }
+      removeChild.detach();
+    }
+  }
+
+  private static void simpleRemoveAtIndex(int index, Element parent, boolean removeOneLineFromMultiline) {
+    Content contentToRemove = parent.getContent(index);
+    String text = null;
+    if (removeOneLineFromMultiline) {
+      text = contentToRemove.getValue().replaceFirst("\n", "");
+      // remove indention if ancestor is no newline text or
+      // if successor is newline text
+      if ((index + 1 < parent.getContent().size() && hasNewlines(parent.getContent(index + 1))) ||
+              (index > 0 && !hasNewlines(parent.getContent(index - 1)))) {
+        LOG.debug("       VALUE: {}", contentToString(contentToRemove));
+        text = text.replaceAll(" ", "");
+      }
+    }
+    parent.removeContent(index);
+    contentToRemove.detach();
+    if (null != text) {
+      parent.addContent(index, new Text(text));
+      LOG.debug("       ML  new => " + contentToString(parent.getContent(index)));
+    }
+  }
+
+  private static int removeCommentAtIndex(int index, Element parent) {
+    Content content = parent.getContent(index);
+    if (isComment(content)) {
+      //remove comment
+      LOG.debug("remove comment => {}", contentToString(content));
+      simpleRemoveAtIndex(index, parent, false);
+      int prevIndex = index - 1;
+      if (prevIndex >= 0 && isNewline(parent.getContent(prevIndex))) {
+        LOG.debug("remove NL      => {}", contentToString(parent.getContent(prevIndex)));
+        simpleRemoveAtIndex(prevIndex, parent, false);
+        return prevIndex - 1;
+      } else if (prevIndex >= 0 && isMultiNewLine(parent.getContent(prevIndex))) {
+        LOG.debug("remove ML      => {}", contentToString(parent.getContent(prevIndex)));
+        simpleRemoveAtIndex(prevIndex, parent, true);
+        return -1;
+      }
+      return index - 1;
+    }
+    return index;
   }
 
   /**
@@ -335,5 +382,241 @@ public final class JDomUtils {
       }
     }
     return tagElement;
+  }
+
+  static boolean isBlankLineBeforeElement(String name, Element root) {
+    List<String> elementOrder = JDomCfg.getInstance().getElementOrder(root.getName());
+    return elementOrder != null && elementOrder.get(max(0, elementOrder.indexOf(name) - 1)).equals("");
+  }
+
+  /**
+   * Tries to detect the indentation that is used within the given element and returns it.
+   * <p>
+   * The method actually returns all characters (supposed to be whitespaces) that occur after the last linebreak in a
+   * text element that is followed by an XML element.
+   *
+   * @param element the element whose contents should be used to detect the indentation.
+   * @return the detected indentation or {@code null} if not indentation can be detected.
+   */
+  public static String detectIndentation(Element element) {
+    String indent = null;
+
+    String indentCandidate = null;
+    for (Content childElement : element.getContent()) {
+      if (childElement instanceof Text) {
+        String text = ((Text) childElement).getText();
+        int lastLsIndex = StringUtils.lastIndexOfAny(text, new String[]{"\n", "\r"});
+        if (lastLsIndex > -1) {
+          indentCandidate = text.substring(lastLsIndex + 1);
+        }
+      } else if (indentCandidate != null) {
+        if (childElement instanceof Element) {
+          indent = indentCandidate;
+          break;
+        } else {
+          indentCandidate = null;
+        }
+      }
+    }
+
+    if (indent == null) {
+      Parent parent = element.getParent();
+      if (parent instanceof Element) {
+        indent = detectIndentation((Element) parent) + "  ";
+      } else {
+        return "";
+      }
+    }
+
+    return indent;
+  }
+
+  static String contentToString(Content content) {
+    if (content instanceof Element) {
+      return elementToString((Element) content);
+    }
+    if (content instanceof Text) {
+      return textToString((Text) content);
+    }
+    return content.getCType() + " => " + content.getValue().trim();
+  }
+
+  private static String elementToString(Element element) {
+    return element.getCType() + " => <" + element.getName() + "> : " + element.getValue().trim().replaceAll("\n", "\\\\n");
+  }
+
+  private static String textToString(Text text) {
+    String value = text.getValue().replaceAll("\n", "\\\\n");
+    if (isNewline(text)) {
+      return "New Line: '" + value + "'";
+    }
+    if (isMultiNewLine(text)) {
+      return "Multi New Line: '" + value + "'";
+    }
+    return text.getCType() + " => " + value;
+  }
+
+  /**
+   * Check if content is a newline.
+   *
+   * @param content the content to check
+   * @return boolean
+   */
+  static boolean hasNewlines(Content content) {
+    if (content instanceof Text) {
+      Text text = (Text) content;
+      return StringUtils.countMatches(text.getValue(), "\n") >= 1;
+    }
+    return false;
+  }
+
+  /**
+   * Check if content is a newline.
+   *
+   * @param content the content to check
+   * @return boolean
+   */
+  static boolean isNewline(Content content) {
+    if (content instanceof Text) {
+      Text text = (Text) content;
+      return StringUtils.countMatches(text.getValue(), "\n") == 1;
+    }
+    return false;
+  }
+
+  /**
+   * Check if content is a multiline.
+   *
+   * @param content the content to check
+   * @return boolean
+   */
+  static boolean isMultiNewLine(Content content) {
+    if (content instanceof Text) {
+      Text text = (Text) content;
+      return StringUtils.countMatches(text.getValue(), "\n") > 1;
+    }
+    return false;
+  }
+
+  /**
+   * Get all direct children of the element.
+   *
+   * @param element the element to consider.
+   * @return List of {@link Content}s
+   */
+  private static List<Content> getDirectContents(Element element) {
+    // get all direct children
+    List<Content> children = new ArrayList<>();
+    for (Content descendant : element.getParentElement().getDescendants()) {
+      if (!descendant.getParent().equals(element)) {
+        // Only consider direct children
+        continue;
+      }
+      children.add(descendant);
+    }
+    return children;
+  }
+
+  /**
+   * Check if content is a comment.
+   *
+   * @param content the content to check
+   * @return boolean
+   */
+  static boolean isComment(Content content) {
+    return content instanceof Comment;
+  }
+
+  /**
+   * Get all comments attached to the element.
+   *
+   * @param element the element to consider.
+   * @return List of {@link Comment}s
+   */
+  static List<Content> getAttachedComments(Element element) {
+    List<Content> contents = new ArrayList<>();
+    List<Content> siblings = getDirectContents(element.getParentElement());
+    int indexOfElement = siblings.indexOf(element);
+    for (int i = indexOfElement - 1; i >= 0; i--) {
+      if (isNewline(siblings.get(i))) {
+        contents.add(siblings.get(i));
+        i--;
+      }
+      if (i >= 0 && siblings.get(i) instanceof Comment) {
+        contents.add(siblings.get(i));
+        continue;
+      }
+      if (i >= 0 && isMultiNewLine(siblings.get(i))) {
+        contents.add(siblings.get(i));
+      }
+      break;
+    }
+    return contents;
+  }
+
+  /**
+   * Remove all empty profiles and profile tag.<br>
+   * Empty is defined as for tag:
+   * <ul>
+   * <li>profiles: there are no profile tags</li>
+   * <li>profile: Either there are only the tags activation and id present or all other tags are empty</li>
+   * </ul>
+   * Thus, empty tag may contain comments which will be removed as well.
+   *
+   * @param rootElement the root element.
+   */
+  public static void cleanupEmptyProfiles(Element rootElement) {
+    IteratorIterable<Element> filteredElements = rootElement.getDescendants(new ElementFilter(POM_ELEMENT_PROFILES));
+    List<Element> profiles = new ArrayList<>();
+    for (Element profilesElement : filteredElements) {
+      profiles.add(profilesElement);
+    }
+    for (Element profilesElement : profiles) {
+      removeElementWithEmptyChildren(profilesElement, POM_ELEMENT_PROFILE, Arrays.asList(JDomCfg.POM_ELEMENT_ID, JDomCfg.POM_ELEMENT_ACTIVATION));
+      if (!profilesElement.getDescendants(new ElementFilter(POM_ELEMENT_PROFILE)).hasNext()) {
+        JDomUtils.removeChildElement(rootElement, profilesElement);
+      }
+    }
+  }
+
+  /**
+   * Remove all empty children with tag name from parent element.<br>
+   * The child is considered as empty if the child has either:
+   * <ul>
+   * <li>no children itself or</li>
+   * <li>only children which are ignored or/and</li>
+   * <li>empty children itself which are not ignored</li>
+   * </ul>
+   *
+   * @param parent         the parent element.
+   * @param tagName        the tag name to search for.
+   * @param ignoreChildren List of children tag names which are ignored when searching for child elements.
+   */
+  private static void removeElementWithEmptyChildren(Element parent, String tagName, List<String> ignoreChildren) {
+    // Example:
+    // parent === 'profiles' Element
+    // tagName === 'profile'
+    // ignoreChildren === ['id', 'activation']
+
+    // filteredElements === direct children of parent which are of type Element and matching 'tagName'
+    IteratorIterable<Element> filteredElements = parent.getDescendants(new ElementFilter(tagName));
+
+    List<Content> contentToBeRemoved = new ArrayList<>();
+    for (Element filteredElement : filteredElements) {
+      boolean empty = true;
+      for (Element child : filteredElement.getChildren()) {
+        if (!ignoreChildren.contains(child.getName()) && child.getChildren().size() > 0) {
+          empty = false;
+          break;
+        }
+      }
+      if (empty) {
+        contentToBeRemoved.add(filteredElement);
+        contentToBeRemoved.addAll(getAttachedComments(filteredElement));
+      }
+    }
+    for (Content elementToBeRemoved : contentToBeRemoved) {
+      JDomUtils.removeChildContent(parent, elementToBeRemoved);
+    }
   }
 }
